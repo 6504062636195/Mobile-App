2dbccf507b20a87ffc51afe1dac0ecea2004d2ff
6504062636195 <153052539+6504062636195@users.noreply.github.com>
2024-12-24 00:23:09 +0700

    latest change

--- FlutterProject/lib/main.dart
+++ FlutterProject/lib/main.dart
@@ -18,323 +18,177 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Nominee MC Donald'),
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-              debugPrint('leading icon pressed');
-            },
-          ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.shopping_cart),
-              onPressed: () {
-                debugPrint('leading icon pressed');
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.info),
-              onPressed: () {
-                debugPrint('leading icon pressed');
-              },
-            ),
-          ],
+          title: const Text('Electric Car Charging App'),
         ),
-        body: Center(
+        body: const Padding(
+          padding: EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.network(
-                  'https://logos-world.net/wp-content/uploads/2020/04/McDonalds-Logo.png'),
-              const Text(
-                ('Thank you for your service'),
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                ('This is you bill'),
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              /*
-              Image.asset(
-                'McDonalds-Logo.png',
-                width: 120,
-              ),
-              
-              IconButton(
-                icon: const Icon(Icons.favorite_border),
-                onPressed: () {
-                  debugPrint('leading icon pressed');
-                },
-              ),
-              */
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 20),
+              Row(
                 children: [
-                  Icon(
-                    Icons.calendar_month,
-                    size: 30,
-                  ),
-                  Text(
-                    'Date',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
                   SizedBox(
-                    width: 150,
+                    width: 120,
+                    child: Text(
+                      'Current SOC%',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                  Text(
-                    '11 Dec 2024',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter current SOC%',
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 16),
+              Row(
                 children: [
-                  Icon(
-                    Icons.food_bank,
-                    size: 30,
-                  ),
-                  Text(
-                    'Branch',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
                   SizedBox(
-                    width: 170,
+                    width: 120,
+                    child: Text(
+                      'Target SOC%',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                  Text(
-                    'Namek',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter target SOC%',
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 16),
+              Row(
                 children: [
-                  Icon(
-                    Icons.fastfood_sharp,
-                    size: 30,
-                  ),
-                  Text(
-                    'Menu',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
                   SizedBox(
-                    width: 90,
+                    width: 120,
+                    child: Text(
+                      'Chg rate A',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                  Text(
-                    'Big size french fries',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter Charge rate A',
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 16),
+              Row(
                 children: [
-                  Icon(
-                    Icons.money,
-                    size: 30,
-                  ),
-                  Text(
-                    'Total',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
                   SizedBox(
-                    width: 170,
+                    width: 120,
+                    child: Text(
+                      'Volt',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                  Text(
-                    '10 Dollars',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter Volt V = S/T',
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 16),
+              Row(
                 children: [
-                  Icon(
-                    Icons.money,
-                    size: 30,
-                  ),
-                  Text(
-                    'Services Charge',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
                   SizedBox(
-                    width: 100,
+                    width: 120,
+                    child: Text(
+                      'w chg',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                  Text(
-                    '2 Dollars',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter w chg (float number)',
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 16),
+              Row(
                 children: [
-                  Icon(
-                    Icons.money,
-                    size: 30,
-                  ),
-                  Text(
-                    'Total Price',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
                   SizedBox(
-                    width: 130,
+                    width: 120,
+                    child: Text(
+                      'Chg times (hrs)',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                  Text(
-                    '12 Dollars',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter Charge Time (Hours)',
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 16),
+              Row(
                 children: [
-                  Text(
-                    'Thank you for services',
-                    style:
-                        TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                  SizedBox(
+                    width: 120,
+                    child: Text(
+                      'Bat kWh',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter Bat kWh',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    child: Text(
+                      'Eff%',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter Efficiency Rate',
+                      ),
+                    ),
                   ),
                 ],
               ),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {}, child: Icon(Icons.shopping_cart)),
       ),
     );
   }
 }
-
-/*
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          appBarTheme: const AppBarTheme(color: Colors.lightBlueAccent)),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('CS app'),
-        ),
-        body: Text(' Hello, World!'),
-      ),
-    ),
-  );
-}
-*/
-
-/*
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Welcome to my mobile application'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              // 'You have pushed the button this many times:',
-              'Countdown Minus 1',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-ElevatedButton(
-                    onPressed: () {
-                      debugPrint(
-                          'Never gonna give you up, Never gonna let you down');
-                    },
-                    child: const Text(
-                      'Do not click',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-*/
